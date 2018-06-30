7f306db3e5e24fc8135bc083c22ed6af92b16122
Ihor Murzich <murzichigor@outlook.com>
2018-07-01 00:19:12 +0300

    Added ReactPlayer from npm.

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Player.css'
 import PlaybackControlsBar from "./PlaybackControlsBar";
 import Seekbar from "./Seekbar";
+import FilePlayer from "react-player/lib/players/FilePlayer";
 
 function Player({ currentSong }) {
   return (
     <div className="Player-wrapper">
+      <FilePlayer style={{display: 'none'}} url={currentSong.preview} playing={true}/>
       <main className="Player">
         <img src={currentSong.album.cover_medium} alt="album-art" className="Player-albumArt" />
         <div className="Player-track">
