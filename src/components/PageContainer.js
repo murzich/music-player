fab09b41bc8be0b1b53d9f1a186e1d4e2520c0b1
Ihor Murzich <murzichigor@outlook.com>
2018-07-01 14:49:18 +0300

    Added axios & fetching data from Deezer api through CORS-Anywhere.

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Page from "./Page";
 import Playlist from "./Playlist";
 import PlayerContainer from "./PlayerContainer";
-import * as mock from "../mock";
+import * as mockResponse from '../mock';
+
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+// const crossOrigin = 'http://crossorigin.me/';
+const mockUrl = 'http://api.deezer.com/search/track?q=artist:"system of a down"&limit=10';
 
 class PageContainer extends Component {
+
+
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentSong: {album: {}, artist: {}},
+      songsList: [],
+      loading: false,
+    };
   }
 
-  songsList = mock.data;
+  componentDidMount() {
+    this.setState({loading: true});
+    axios.get(corsAnywhere+mockUrl)
+      .then(response => {
+        this.setState({
+          songsList: response.data.data,
+          currentSong: response.data.data[0],
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        // TODO: Used if cors-anywhere fails
+        this.setState({
+          songsList: mockResponse.data.data,
+          currentSong: mockResponse.data.data[0],
+          loading: false,
+        });
+      });
+  }
 
   render() {
     return (
       <Page>
-        <Playlist songs={this.songsList}/>
-        <PlayerContainer currentSong={this.songsList[0]}/>
+        <Playlist songs={this.state.songsList} loading={this.state.loading}/>
+        <PlayerContainer currentSong={this.state.currentSong}/>
       </Page>
     );
   }
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Playlist.css'
 import SongsList from "./SongsList";
 
-function Playlist({ songs }) {
+function Playlist({ songs, loading }) {
   return (
     <aside className="Playlist">
       Playlist component
+      {loading && (<div>Loading...</div>)}
       <SongsList songs={songs} />
     </aside>
   );
