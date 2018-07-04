ab639ca7b8c928ef9453bca17d77e5ba2ebffb35
Ihor Murzich <murzichigor@outlook.com>
2018-07-04 03:23:22 +0300

    Cleaned up playbackButtons array. Replaced button mapping function by const.

--- src/components/player/PlaybackControlsBar.js
+++ src/components/player/PlaybackControlsBar.js
@@ -8,51 +8,41 @@ function PlaybackControlsBar(props) {
   const playbackButtons = [
     {
       name: 'Previous',
-      shortName: 'prev',
       icon: 'fast-backward',
       callback: props.onPrev,
     },
     {
       name: 'Back',
-      shortName: 'back',
       icon: 'backward',
       callback: props.onStepBack,
     },
     {
-      name: (props.playing) ? 'Pause' : 'Play',
-      shortName: (props.playing) ? 'pause' : 'play',
+      name: 'Play/Pause',
       icon: (props.playing) ? 'pause' : 'play',
-      altImg: '#control-pause',
       callback: props.onPlayPause,
     },
     {
       name: 'Forward',
-      shortName: 'fwd',
       icon: 'forward',
       callback: props.onStepForward,
     },
     {
       name: 'Next',
-      shortName: 'next',
       icon: 'fast-forward',
       callback: props.onNext,
     },
   ];
 
-  const collectButtons = (buttonsArray) => {
-    return buttonsArray.map(
-      (button, i) => (
-        withFontAwesome(Button, i, button.callback, button.icon)
-      )
-    );
-  };
+  const buttons = playbackButtons.map(
+    (button) => withFontAwesome(Button, button.name, button.callback, button.icon)
+  );
 
   return (
     <div
        // TODO: temporary styles
       style={{ backgroundColor: 'rgba(255,255,255,.5)', padding: '1rem', borderRadius: '2rem' }}
     >
-      {collectButtons(playbackButtons)}
+      {buttons}
     </div>
   );
 }
