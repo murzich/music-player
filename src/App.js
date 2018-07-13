117e101589a08440e01b17c9089b5a938afb7806
Ihor Murzich <murzichigor@outlook.com>
2018-07-13 21:39:43 +0300

    Extracted PrivateRoute HoC for AuthGuard routes.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PageContainer from './containers/PageContainer';
 import LoginPage from './components/login/LoginPage';
 import LoginSuccess from './containers/LoginSuccess';
+import PrivateRoute from './containers/PrivateRoute';
 
-function App({ isAuthorized }) {
+function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/login-success" component={LoginSuccess} />
         <Route path="/login" component={LoginPage} />
-        <Route
-          path="/"
-          render={props => (
-            (isAuthorized) ?
-              <PageContainer {...props} /> :
-              <Redirect to="/login" />
-            )}
-        />
+        <PrivateRoute path="/" component={PageContainer} />
       </Switch>
     </BrowserRouter>
   );
 }
 
-App.propTypes = {
-  isAuthorized: PropTypes.bool.isRequired,
-};
-
-const mapStateToProps = (state) => {
-  const { isAuthorized } = state.auth;
-  return {
-    isAuthorized,
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
---
+++ src/containers/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+function PrivateRoute({ component: Component, isAuthorized, ...rest }) {
+  const renderComponent = props => (
+    isAuthorized ?
+      <Component {...props} /> :
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { from: 'props.location' },
+        }}
+      />
+  );
+  return <Route {...rest} render={renderComponent} />;
+}
+
+PrivateRoute.propTypes = {
+  isAuthorized: PropTypes.bool,
+  component: PropTypes.node.isRequired,
+};
+PrivateRoute.defaultProps = {
+  isAuthorized: false,
+};
+
+const mapStateToProps = ({ auth }) => ({
+  isAuthorized: auth.isAuthorized,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
